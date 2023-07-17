db5fae46e5740eeecdf2da996b28d9ef4b49bb7e
Marwan Rahim <marwan820@Marwan>
2023-07-17 17:52:44 -0500

    fixed the map link code

--- Notes.md
+++ Notes.md
@@ -1,13 +1,18 @@
 # Goals
-- [] Create a page where a user can search a counries information by entering a country name in a search field
-- [] Country information 
+- [x] Create a page where a user can search a counries information by entering a country name in a search field
 - 
 
 
 
 # TODO
 - [x] Create a search bar to enter countries name 
-- [x]
+- [] Write a catch message, when a user input doesnt match with database
+- [] Style it, give it some color and flavor 
+- [] Separate the images 
+- [] Find out if its better to give attributes on html or javascript
+- [] map link issue still not working
+- [] click on header to reset the page
+- 
 # Project Requirements:
 - [x]   Your app must be an HTML/CSS/JS frontend that accesses data from a public API or from your own API built using json-server and a db.json file.
 - [x]   Your API or db.json should return a collection of at least 5 objects with each object having at least 3 attributes.
--- main.js
+++ main.js
@@ -14,12 +14,16 @@ function getSelection(e){
     .then(data => iteratorFunction(data))}
     //.catch(error =>  
        //const errorMessage = `Country not found: ${error}`)}
+    
+
 
 
 function iteratorFunction(data){
     const countryContainer = document.querySelector(".country-container")
     countryContainer.replaceChildren()
     data.forEach(country => {
+
+        console.log(country.altSpellings[1])
    
 const title = document.createElement("h2")
 title.id = "title"
@@ -31,10 +35,14 @@ flagImage.src = country.flags.svg
 flagImage.rel = country.flags.rel
 flagImage.style.width = "50%"
 
+
 const coatOfArms = document.createElement("img")
 coatOfArms.id = "img coatOfArms"
 coatOfArms.src = country.coatOfArms.svg
-coatOfArms.style.width = "30%"
+coatOfArms.style.width = "20%"
+const captionCoatOfArms = document.createElement("figcaption")
+captionCoatOfArms.classList = "coatOfArms-text"
+captionCoatOfArms.textContent = "Coat of Arms"
 
 
 // Section for information about countries 
@@ -56,13 +64,11 @@ currenciesTag.id = "currencies"
 const currencyKey = Object.keys(country.currencies)[0]
 const currencyName = country.currencies[currencyKey].name
 currenciesTag.textContent = `Currency: ${currencyName}`
-console.log(currencyKey)
 
 const languages = document.createElement("p")
 languages.id = "languages"
 const languagesKey = Object.keys(country.languages)[0]
 const languageValue = country.languages[languagesKey]
-console.log(languageValue)
 languages.textContent = `Majority Language: ${languageValue}`
 
 
@@ -71,14 +77,16 @@ timeZones.id = "timezones"
 timeZones.textContent=  `Time zone ${country.timezones}`
 console.log(country)
 
-const mapLink = document.createElement("p")
+const mapLink = document.createElement("a")
 mapLink.id = "mapLink"
-mapLink.href = country.maps
-mapLink.rel = "Google map it"
+mapLink.href = country.maps.googleMaps
+mapLink.textContent = "Google map"
+mapLink.target = "blank"
 
+console.log(mapLink)
 // Append
-countryContainer.append(title,flagImage,coatOfArms)
-dataText.append(capital,continent,currenciesTag,mapLink,timeZones,languages)
+countryContainer.append(title,flagImage,coatOfArms,captionCoatOfArms)
+dataText.append(capital,continent,languages,currenciesTag,timeZones,mapLink,)
 countryContainer.append(dataText)})}
     
 
